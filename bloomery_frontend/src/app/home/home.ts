6131e99c4d7430217712e4a233cfeb532f3011ad
AaronMV1 <aaronmorivaldivia@gmail.com>
2026-03-17 14:25:36 -0500

    commit 18/03/26

--- bloomery_frontend/src/app/home/home.ts
+++ bloomery_frontend/src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +6,39 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements OnInit {
 
+  banners = [
+    {
+      imageUrl: 'https://ethic.es/wp-content/uploads/2023/03/imagen-640x384.jpg',
+      url: 'https://ethic.es/wp-content/uploads/2023/03/imagen-640x384.jpg'
+    },
+    {
+      imageUrl: 'https://ethic.es/wp-content/uploads/2023/03/imagen-640x384.jpg',
+      url: 'https://ethic.es/wp-content/uploads/2023/03/imagen-640x384.jpg'
+    },
+    {
+      imageUrl: 'https://ethic.es/wp-content/uploads/2023/03/imagen-640x384.jpg',
+      url: 'https://ethic.es/wp-content/uploads/2023/03/imagen-640x384.jpg'
+    }
+  ];
+
+  currentIndex = 0;
+
+  // Tiempo configurable en milisegundos (por defecto 3 segundos)
+  bannerInterval: number = 3000;
+
+  ngOnInit() {
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.banners.length;
+    }, this.bannerInterval);
+  }
+
+  get currentImage() {
+    return this.banners[this.currentIndex].imageUrl;
+  }
+
+  get currentUrl() {
+    return this.banners[this.currentIndex].url;
+  }
 }
